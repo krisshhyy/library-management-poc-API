717bb48830fef69978845576945e348a21033b43
Krishnendu Nandi <k.nandi@accenture.com>
2025-01-16 19:53:29 +0530

    Add project files.

---
+++ NandiAPI/Controllers/LibraryController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NandiAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LibraryController : ControllerBase
+    {
+    }
+}
